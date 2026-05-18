f6d21eb5458f2c2c8bd89edd988eeed6ac629c60
Szymon <szymonngos@gmail.com>
2026-05-18 15:48:52 +0200

    add TripImage service impl, cleanup

--- src/main/java/com/szymong/trip_planner_api/tripImage/repository/TripImageRepository.java
+++ src/main/java/com/szymong/trip_planner_api/tripImage/repository/TripImageRepository.java
@@ -3,5 +3,8 @@ package com.szymong.trip_planner_api.tripImage.repository;
 import com.szymong.trip_planner_api.tripImage.TripImage;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface TripImageRepository extends JpaRepository<TripImage, Long> {
+  List<TripImage> findByTripId(Long tripId);
 }
