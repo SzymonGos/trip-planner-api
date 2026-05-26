a6f7a865c67cfd0995f5b7bcfd868f9efbe4060d
Szymon <szymonngos@gmail.com>
2026-05-26 12:00:12 +0200

    cleanup

--- src/main/java/com/szymong/trip_planner_api/tripImage/repository/TripImageRepository.java
+++ src/main/java/com/szymong/trip_planner_api/tripImage/repository/TripImageRepository.java
@@ -1,6 +1,7 @@
 package com.szymong.trip_planner_api.tripImage.repository;
 
 import com.szymong.trip_planner_api.tripImage.TripImage;
+import com.szymong.trip_planner_api.tripImage.dto.TripImageResponse;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
